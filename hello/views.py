273b56e9fe1eacce73f01a6c451705e22972ce38
Administrator <Administrator@1000-RLS.senox.local>
2018-06-26 15:08:38 -0500

    New move to my repo

--- hello/views.py
+++ hello/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+import requests
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
+   
 
 
 def db(request):
